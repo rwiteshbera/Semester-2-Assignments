dfaa1354cb893e58e0869dfc5b3b5dc8a15ef298
rwiteshbera <rwiteshbera@gmail.com>
2021-07-05 15:40:19 +0530

    Updated LAB 11

--- LAB-11_JULY-05/Problem-06.c
+++ LAB-11_JULY-05/Problem-06.c
@@ -1,15 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+    printf("Enter the string : \n");
+    char s[200];
+    fgets(s,200,stdin);
+    int count = 0, vowels[5] = {};
 
-int main(){
-    char str[100];
-    printf("Enter a string: ");
-    fgets(str,100,stdin);
-
-    int arr[128];
-    for(int i=0; str[i]!='\0'; i++)
+    for (int i = 0; s[i] != '\0'; i++)
     {
+        if (s[i] == 'a')
+            vowels[0]++;
 
+        if (s[i] == 'e')
+            vowels[1]++;
+
+        if (s[i] == 'i')
+            vowels[2]++;
+
+        if (s[i] == 'o')
+            vowels[3]++;
+
+        if (s[i] == 'u')
+            vowels[4]++;
+
+        if (s[i] == ';')
+            s[i] = '@';
     }
 
+    for (int i = 0; i < 5; i++)
+    {
+        if (vowels[i] > 0)
+            count++;
+    }
+
+    printf("Number of Vowels (non-repeated) = %d\n", count);
+    printf("String after replacement = %s", s);
     return 0;
 }
